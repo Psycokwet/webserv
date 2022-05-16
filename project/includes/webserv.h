505a9cf828ea9c7db065ddeca377569f9b5defab
mida <mida@student.42.fr>
2022-05-16 13:37:57 +0200

    include/utils

--- project/includes/webserv.h
+++ project/includes/webserv.h
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   webserv.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: idamouttou <idamouttou@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 22:03:00 by scarboni          #+#    #+#             */
-/*   Updated: 2022/03/19 22:14:43 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/05/05 14:08:58 by idamouttou       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEBSERV_H
 # define WEBSERV_H
+
 # include <stdlib.h>
 //malloc free
 # include <unistd.h>
@@ -21,6 +22,14 @@
 # include <sys/select.h>
 # include <sys/time.h>
 # include <sys/types.h>
+//Bytes endian conversion
+# define BYTE_0 0xff000000
+# define BYTE_1 0x00ff0000
+# define BYTE_2 0x0000ff00
+# define BYTE_3 0x000000ff
+
+# define ft_htonl ft_ntohl
+# define ft_htons ft_ntohs
 //select
 # include <poll.h>
 //poll
@@ -34,6 +43,16 @@
 //inet_addr 
 #include <fcntl.h>
 //fcntl
+
+//ntoh / hton
+unsigned short				ft_ntohs(unsigned short i);
+unsigned short				ft_htons(unsigned short i);
+
+unsigned int				ft_htonl(unsigned int i);
+unsigned int                ft_ntohl(unsigned int i);
+
+
+
 
 # include <iostream>
 #endif
